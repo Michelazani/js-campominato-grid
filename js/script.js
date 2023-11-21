b004c4b9f3998b959f6d45ceba5b0052ce28ab29
Michelazani <michi.zani220@gmail.com>
2023-11-21 17:23:19 +0100

    aggiunto click

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 
+// creo variabile button
+const buttonClick = document.querySelector('button');
+
+// richiamo il button al click
+buttonClick.addEventListener('click', function(){
+    // creo ciclo di 100 numeri
+    for (let i= 1; i <=100; i++){
+
+    }
+}
+
+// do padre al figlio nel ciclo for
+const mainContentEl = document.querySelector('.wrapper');
+
+// funzione
+function newSquare (){
+    const newElement = document.createElement ('article');
+    newElement.classList.add ('square');
+    return newElement;
+});
