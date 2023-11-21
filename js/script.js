69c77eb2f7450c6f6caa5e548d2cbe0c3351e82d
Michelazani <michi.zani220@gmail.com>
2023-11-21 19:41:00 +0100

    console log del numero su cui faccio click

--- js/script.js
+++ js/script.js
@@ -3,23 +3,37 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 
-// creo variabile button
+// creo variabile button                        OKAY
 const buttonClick = document.querySelector('button');
 
-// richiamo il button al click
+// do padre al figlio nel ciclo for             OKAY
+const mainContentEl = document.querySelector('.wrapper');
+ 
+// richiamo il button al click                 !!!!!!!!!! MEZZO OKAY!!!!!!!!!
+// li crea, 100 ogni volta e NON VA BENE 
 buttonClick.addEventListener('click', function(){
     // creo ciclo di 100 numeri
     for (let i= 1; i <=100; i++){
-
+        const currentSquare = newSquare();
+        const squareContent= i;
+        mainContentEl.appendChild(currentSquare);
+        // numeri da inserire con squareContent
+        currentSquare.innerHTML += `<span> ${squareContent} </span>`;
+        // apro altro click, quando clicco su cella, si colora DA INSERIRE NEL FOR ALTRIMENTI NON FUNZIONA
+        currentSquare.addEventListener('click', function(){
+        currentSquare.classList.toggle('blue')
+        console.log(squareContent)
+        });
     }
-}
+// chiuso ciclo for
+});
+// chiusura click
 
-// do padre al figlio nel ciclo for
-const mainContentEl = document.querySelector('.wrapper');
 
-// funzione
+
+// funzione                                     OKAY
 function newSquare (){
     const newElement = document.createElement ('article');
     newElement.classList.add ('square');
     return newElement;
-});
+};
